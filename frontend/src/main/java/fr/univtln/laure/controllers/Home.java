6d9d6f124d809085770d04614f6d281f84b8b519
Apierriecube <apierriecube.666@gmail.com>
2025-05-07 11:17:17 +0200

    Create last page (MoviePage) and add deconnexion button

--- frontend/src/main/java/fr/univtln/laure/controllers/Home.java
+++ frontend/src/main/java/fr/univtln/laure/controllers/Home.java
@@ -1,12 +1,57 @@
 package fr.univtln.laure.controllers;
 
+import java.io.IOException;
+import java.net.URL;
+
+import fr.univtln.laure.utils.SceneChanger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Home {
+
+    @FXML
+    private Button deconnexionButton;
 
     private static int IdConnexion;
 
-    public void setIdConnexion(int id) {
-        this.IdConnexion = id;
+    public static void setIdConnexion(int id) {
+        IdConnexion = id;
     }
 
     public static int getIdConnexion() {return IdConnexion;}
+
+    public void handleDisconnect(){
+        try {
+            URL fxmlUrl = getClass().getResource("/views/login.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            Scene scene = new Scene(loader.load());
+
+            this.setIdConnexion(0);
+
+            Stage stage = (Stage) deconnexionButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            SceneChanger.setWindow(stage, "login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openMovie(){
+        try {
+            URL fxmlUrl = getClass().getResource("/views/moviePage.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            Scene scene = new Scene(loader.load());
+
+            Stage stage = (Stage) deconnexionButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("MoviePage");
+            SceneChanger.setWindow(stage, "moviePage");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- frontend/src/main/java/fr/univtln/laure/controllers/MoviePage.java
+++ frontend/src/main/java/fr/univtln/laure/controllers/MoviePage.java
@@ -1,5 +1,35 @@
 package fr.univtln.laure.controllers;
 
+import java.io.IOException;
+import java.net.URL;
+
+import fr.univtln.laure.utils.SceneChanger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MoviePage {
+
+    @FXML
+    private Button deconnexionButton;
+
+    public void handleDisconnect(){
+        try {
+            URL fxmlUrl = getClass().getResource("/views/login.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            Scene scene = new Scene(loader.load());
+
+            Home.setIdConnexion(0);
+
+            Stage stage = (Stage) deconnexionButton.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            SceneChanger.setWindow(stage, "login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
