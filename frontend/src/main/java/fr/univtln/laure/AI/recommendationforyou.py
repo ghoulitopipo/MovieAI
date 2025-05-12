34ecd1e060b37850598cdb6a75ec73b2ecf43ad3
Stephane Rossi <73294300+Makrayd@users.noreply.github.com>
2025-05-12 09:35:28 +0200

    Update recommendationforyou.py

--- frontend/src/main/java/fr/univtln/laure/AI/recommendationforyou.py
+++ frontend/src/main/java/fr/univtln/laure/AI/recommendationforyou.py
@@ -1,49 +1,56 @@
-import recommendationflask
-#Return the not noted movies which it is in the genre and most averaged-noted  
+#import recommendationflask
+import write
+#Return the not noted movies which it is in the genre and most averaged-noted
 
-#Requete SQL SELECT Movies.* FROM Ratings Jointure Movies WHERE user.id=u AND rating = 0 AND genre=genre 
-#TO DO
-def Notrated(u=0, genre="Action"):
-    return recommendationflask.unrated_movies(u,genre)
+#Requete SQL SELECT Movies.* FROM Ratings Jointure Movies WHERE user.id=u AND rating = 0 AND genre=genre
+def Notratedonegenre(u=0, genre="Action",nbre=1):
+    return []
+    #return recommendationflask.unrated_movies(u,genre,nbre)
+
+
 
 
 #Return the matrix genre/average note/count of user [genre1,average1,count1],[genre2,average2,count2] 
 
 # Requete SQL : SELECT genre, avg(rating), count() FROM Ratings Jointure Movies WHERE user.id=u AND NOT(rating = 0) GROUP BY genre
-#TO DO
-def Matrixgenrerating(u=0): 
-    return recommendationflask.ratings_by_genre(u)
+def Matrixgenrerating(u=0):
+    return [["Action",3,3],["Animation",1.9,15],["War",4.2,20],["Western",5,2]]
+    #return recommendationflask.ratings_by_genre(u)
 
 
-CW=1 # count weight
-RW=1 # rating weight 
 
 
 u=0 # user 
-M=Matrixgenrerating(u)
-i=0 
-j=0
+
+def Likedgenres(u=0):
+    CW=1 # count weight
+    RW=10 # rating weight 
+    N=100 # quantity of recommended movies
+    M=Matrixgenrerating(u)
 
     
-G=[["Action",0],["Adventure",0],["Animation",0],["Children's",0],["Comedy",0],["Crime",0],["Documentary",0],["Crime",0]
-,["Documentary",0],["Drama",0],["Fantasy",0],["Film-Noir",0],["Horror",0],["Musical",0],["Mystery",0],["Romance",0],["Sci-Fi",0]
-,["Thriller",0],["War",0],["Western",0]]
+    G=[["Action",0],["Adventure",0],["Animation",0],["Children's",0],["Comedy",0],["Crime",0],["Documentary",0],["Crime",0]
+    ,["Documentary",0],["Drama",0],["Fantasy",0],["Film-Noir",0],["Horror",0],["Musical",0],["Mystery",0],["Romance",0],["Sci-Fi",0]
+    ,["Thriller",0],["War",0],["Western",0]]
 
 
+    s=0
+    i=0
+    j=0
+    while i < len(M):
+        if (G[j][0]==M[i][0]):
+            G[j][1]=M[i][1]*RW+M[i][2]*CW
+            s+=G[j][1]
+            i+=1
+        j+=1
+    j=0
+    while j < len(G):
+        G[j][1]=(G[j][1]*N)//s
+        j+=1
+    G.sort(key=lambda x: x[1],reverse=True)
+    return G
 
-while i < len(M):
-    if (G[j][0]==M[i][0]):
-        G[j][1]=M[i][1]*RW*M[i][2]*CW
-        i+=1
-    j+=1
 
-maxg=0
-maxi=G[0][1]
-for j in range(1,len(G)):
-    if G[j][1]>maxi:
-        maxi=G[j][1]
-        maxg=j
+print(Likedgenres())
+print(Notratedonegenre(u,G[maxg][0]))
 
-print(G)
-print(G[maxg][0])
-print(Notrated(u,G[maxg][0]))
