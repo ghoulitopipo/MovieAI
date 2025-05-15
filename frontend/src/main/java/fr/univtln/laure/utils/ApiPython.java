2e2afa55d732a7e1f45cc2a4ef3e58958c9f036c
Stephane Rossi <73294300+Makrayd@users.noreply.github.com>
2025-05-15 09:20:12 +0200

    Update ApiPython.java

--- frontend/src/main/java/fr/univtln/laure/utils/ApiPython.java
+++ frontend/src/main/java/fr/univtln/laure/utils/ApiPython.java
@@ -1,5 +1,20 @@
 package fr.univtln.laure.utils;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ApiPython {
-    
+
+    public static String getRecommendationIndiGenreJson(int userId) {
+        ProcessBuilder pb = new ProcessBuilder("python3", "recommendationindiGenre.py", String.valueOf(userId));
+        Process process = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line);
+        }
+
+        return output.toString();
+    }
 }
