3b5e469609ec234dc01ded75ecfa8557e683a94c
Apierriecube <apierriecube.666@gmail.com>
2025-05-16 09:13:03 +0200

    Create new "branch" Recommendation_Python for better ide gestion and separation of concern.

--- frontend/src/main/java/fr/univtln/laure/utils/ApiPython.java
+++ frontend/src/main/java/fr/univtln/laure/utils/ApiPython.java
@@ -1,18 +1,30 @@
 package fr.univtln.laure.utils;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ApiPython {
 
     public static String getRecommendationIndiGenreJson(int userId) {
         ProcessBuilder pb = new ProcessBuilder("python3", "recommendationindiGenre.py", String.valueOf(userId));
-        Process process = pb.start();
+        Process process = null;
+        try {
+            process = pb.start();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         StringBuilder output = new StringBuilder();
         String line;
-        while ((line = reader.readLine()) != null) {
-            output.append(line);
+        try {
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
         return output.toString();
