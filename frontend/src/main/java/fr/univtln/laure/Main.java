0f7311671d238751f99f195596ef2c4265f2716e
Apierriecube <apierriecube.666@gmail.com>
2025-05-16 11:49:34 +0200

    fix

--- frontend/src/main/java/fr/univtln/laure/Main.java
+++ frontend/src/main/java/fr/univtln/laure/Main.java
@@ -21,7 +21,6 @@ public class Main extends Application{
     }
 
     public static void main(String[] args) throws Exception {
-        ApiPython.RecommendationForYou(1);
         launch(args);
     }
 }
