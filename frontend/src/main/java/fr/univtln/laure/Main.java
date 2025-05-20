0cb19fc4057515e44f2985b0c7d283643f13c80f
Apierriecube <apierriecube.666@gmail.com>
2025-05-20 10:19:39 +0200

    fix mon oubli

--- frontend/src/main/java/fr/univtln/laure/Main.java
+++ frontend/src/main/java/fr/univtln/laure/Main.java
@@ -21,7 +21,6 @@ public class Main extends Application{
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(ApiPython.RecommendationForYou(3));
         launch(args);
     }
 }
